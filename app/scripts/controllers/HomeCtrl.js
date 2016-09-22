a0b1a8a0ec438ee2a8b5a801208221dd8ed58599
mattcheang <cheangmatt@gmail.com>
2016-09-21 17:10:22 -0700

    display chat room

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,9 @@
 (function() {
      function HomeCtrl(Room) {
         this.roomData = Room;
-     }
-  
+         this.currentRoom =  Room.list.$indexFor(num);
+        }
+    
      angular
          .module('blocChat')
          .controller('HomeCtrl', ['Room', HomeCtrl]);
